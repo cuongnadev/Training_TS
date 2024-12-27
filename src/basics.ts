4a66f555b9db84afbc77c6dd379ec351abfddbbf
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2024-12-27 15:25:48 +0700

    #1 basics

---
+++ src/basics.ts
@@ -0,0 +1,18 @@
+const add = (
+    n1: number,
+    n2: number, 
+    showResult: boolean, 
+    title: string
+): void => {
+    let result = n1 + n2;
+    if (showResult) {
+        console.log(title + result);
+    } else {
+        throw new Error('Not show')
+    }
+};
+
+let num1: number;
+num1 = 10;
+const num2 = 9.5;
+add(num1, num2, true, "Sum: ");
