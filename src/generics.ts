02e555bac3f6b4f2a1a6875aba033f74889a6a41
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2024-12-31 23:27:47 +0700

    #12 generics

--- src/generics.ts
+++ src/generics.ts
@@ -1,34 +1,37 @@
 // const names = ['John', 'Coca', 'Lux'];
-const names: Array<string> = ['John', 'Coca', 'Lux'];
+const names: Array<string> = ["John", "Coca", "Lux"];
 
-const promise = new Promise<string> ((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
+const promise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
 });
 
 promise.then((data) => {
-    data.split(' ');
-})
+  data.split(" ");
+});
 
 // Generic Functions
 function merge<T extends object, U>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergeObj = merge<{name: string}, {age: number}>({name: 'John'}, {age: 30});
+const mergeObj = merge<{ name: string }, { age: number }>(
+  { name: "John" },
+  { age: 30 }
+);
 console.log(mergeObj);
 console.log(mergeObj.name);
 
 interface Lengthy {
-  length: number
+  length: number;
 }
 
-function countAndDescribe<T extends Lengthy> (element: T): [T, string] {
-  let descriptions = 'Got no element';
-  if(element.length === 1) {
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptions = "Got no element";
+  if (element.length === 1) {
     descriptions = `Got 1 element`;
-  } else if(element.length > 1) {
+  } else if (element.length > 1) {
     descriptions = `Got ${element.length} elements`;
   }
 
@@ -38,15 +41,17 @@ function countAndDescribe<T extends Lengthy> (element: T): [T, string] {
 // console.log(countAndDescribe({length: 3}));
 // console.log(countAndDescribe([]));
 // console.log(countAndDescribe(['John']));
-console.log(countAndDescribe(['John', 'Doe']));
-
+console.log(countAndDescribe(["John", "Doe"]));
 
 // keyof constraint
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-  return 'Value: ' +  obj[key];
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
 }
 
-console.log(extractAndConvert({name: "Wick"}, 'name'));
+console.log(extractAndConvert({ name: "Wick" }, "name"));
 
 // Generic class
 class DataStorage<T> {
@@ -57,7 +62,7 @@ class DataStorage<T> {
   }
 
   removeItem(item: T) {
-    if(this.data.indexOf(item) === -1) {
+    if (this.data.indexOf(item) === -1) {
       return;
     }
     this.data.splice(this.data.indexOf(item), 1);
@@ -69,24 +74,24 @@ class DataStorage<T> {
 }
 
 const textStorage = new DataStorage<string>();
-textStorage.addItem('Hello');
-textStorage.addItem('World');
-textStorage.removeItem('Hello');
+textStorage.addItem("Hello");
+textStorage.addItem("World");
+textStorage.removeItem("Hello");
 console.log(textStorage.getItems());
 
 // Generic Utility types
 interface CourseGoal {
   title: string;
-  description:  string;
+  description: string;
 }
 
 function createCourseGoal(title: string, description: string): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {}; 
+  let courseGoal: Partial<CourseGoal> = {};
   courseGoal.title = title;
   courseGoal.description = description;
   return courseGoal as CourseGoal;
 }
 
-const majors: Readonly<string[]> = ['NS', 'AI'];
+const majors: Readonly<string[]> = ["NS", "AI"];
 // majors.push('SE'); // push do not exits on type Readonly string[]
 // majors.pop(); //
