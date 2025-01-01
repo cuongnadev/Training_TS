885228390255867a1ce46df46ee8e28b63c9665d
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2025-01-02 00:53:49 +0700

    #13 decorator

--- src/main.ts
+++ src/main.ts
@@ -1,92 +1,250 @@
-// const names = ['John', 'Coca', 'Lux'];
-const names: Array<string> = ['John', 'Coca', 'Lux'];
+// Decorator thông thường
+// function Logger(constructor: Function) {
+//   console.log('Logger executed');
+//   console.log(constructor);
+// }
 
-const promise = new Promise<string> ((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
+// @Logger
+// class Person {
+//   name = 'Max';
+//   constructor() {
+//     console.log('Creating a new person...');
+//   }
+// }
 
-promise.then((data) => {
-    data.split(' ');
-})
+// Output:
+// Logger executed
+// class Person {...}
+// Creating a new person...
 
-// Generic Functions
-function merge<T extends object, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
-}
 
-const mergeObj = merge<{name: string}, {age: number}>({name: 'John'}, {age: 30});
-console.log(mergeObj);
-console.log(mergeObj.name);
-
-interface Lengthy {
-  length: number
-}
-
-function countAndDescribe<T extends Lengthy> (element: T): [T, string] {
-  let descriptions = 'Got no element';
-  if(element.length === 1) {
-    descriptions = `Got 1 element`;
-  } else if(element.length > 1) {
-    descriptions = `Got ${element.length} elements`;
+/** <=========================================================================> */
+// Decorator Factory
+function Logger(logString: string) {
+  console.log('Logger Factory');
+  
+  return function(constructor: Function) {
+    console.log('Rendering logger'); 
+    
+    console.log(logString);
+    console.log(constructor);
   }
-
-  return [element, descriptions];
 }
 
-// console.log(countAndDescribe({length: 3}));
-// console.log(countAndDescribe([]));
-// console.log(countAndDescribe(['John']));
-console.log(countAndDescribe(['John', 'Doe']));
+// Structure
+// 1. Decorator Factory
+function WithTemplate(template: string, hookId: string) {
+  console.log('Template Factory'); // Log khi factory được gọi
 
+  // 2. Decorator Function
+  return function<T extends {new(...args: any[]): {name: string}}>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor { // return new constructor
+      constructor(...args: any[]) {
+        super();
+        console.log('Rendering template'); // Log khi decorator thực thi
 
-// keyof constraint
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-  return 'Value: ' +  obj[key];
-}
-
-console.log(extractAndConvert({name: "Wick"}, 'name'));
-
-// Generic class
-class DataStorage<T> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    if(this.data.indexOf(item) === -1) {
-      return;
+        // 3. Logic bên trong
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
     }
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
   }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Hello');
-textStorage.addItem('World');
-textStorage.removeItem('Hello');
-console.log(textStorage.getItems());
+// Add mutiple Decorator
+@Logger('Logging - Person')
+@WithTemplate('<h1>Hello, my Person object</h1>', 'app')
+class Person {
+  name = 'Max';
+  
+  constructor() {
+    console.log('Creating a new person...');
+  }
+}
 
-// Generic Utility types
-interface CourseGoal {
+const p = new Person();
+
+
+/** <=========================================================================> */
+// Class Decorator
+function Log0(constructor: Function) {
+  console.log('Class Decorator!');
+  console.log('constructor :', constructor);
+}
+// Property Decorator
+function Log1(target: any, propertyName: string | symbol) {
+  console.log('Property Decorator!');
+  console.log('target:', target);
+  console.log('propertyName:', propertyName);
+}
+
+// Accessor Decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor Decorator!');
+  console.log('target:', target);
+  console.log('name:', name);
+  console.log('discriptor:', descriptor);
+}
+
+// Method Decorator
+function Log3(target: any, methodName: string | symbol, descriptor: PropertyDescriptor) {
+  console.log('Method Decorator!');
+  console.log('target:', target);
+  console.log('methodName:', methodName);
+  console.log('descriptor:', descriptor);
+}
+
+// Parameter Decorator
+function Log4(target: any, name: string | symbol, parameterIndex: number) {
+  console.log('Parameter Decorator!');
+  console.log('target:', target);
+  console.log('name:', name);
+  console.log('parameterIndex:', parameterIndex);
+}
+
+@Log0
+class Product {
+  @Log1
+  title: string; // target is Product.prototype
+  // static title: string; // target is Product
+  private _price: number
+
+  @Log2
+  set price(val: number) {
+    if ( val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    // with Parameter Decorator, name is name Method use parameter
+    return this._price * (1 + tax);
+  }
+}
+
+// decorators are executed at design time, not runtime.
+// The decorators for methods, properties, and accessors are executed when the class and its members are defined, before the class's constructor function is executed.
+
+
+/** <=========================================================================> */
+function Autobind (target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value; // Lấy phương thức gốc từ descriptor.
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true, // Cho phép cấu hình lại thuộc tính.
+    enumerable: false, // Không liệt kê thuộc tính trong vòng lặp.
+    get() {
+      // `this` tại đây sẽ trỏ đến instance của lớp (Printer).
+      const boundFn = originalMethod.bind(this); // Gắn chặt `this` vào phương thức gốc.
+      return boundFn;
+    },
+  }
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const printer = new Printer();
+
+const button = document.querySelector('button')!;
+// button.addEventListener('click', printer.showMessage.bind(printer));
+button.addEventListener('click', printer.showMessage);
+
+
+/** <=========================================================================> */
+interface ValidateConfig {
+  [property: string]: {
+    [validatableProp: string]: string[] // ['required', 'positive']
+  }
+}
+
+const registeredValidators: ValidateConfig = {};
+
+function Required(targer: any, propertyname: string) {
+  registeredValidators[targer.constructor.name] = {
+    ...registeredValidators[targer.constructor.name],
+    [propertyname]: ['required']
+  };
+}
+
+function PositiveNumber(targer: any, propertyname: string) {
+  registeredValidators[targer.constructor.name] = {
+    ...registeredValidators[targer.constructor.name],
+    [propertyname]: ['positive']
+  };
+}
+
+function validate(obj: any) {
+  const objConstructor = obj.constructor.name;
+  const validatorsConfig = registeredValidators[objConstructor];
+
+  if (!validatorsConfig) {
+    return true;
+  }
+
+  let isValid = true;
+  for (const prop in validatorsConfig) {
+    for (const validator of validatorsConfig[prop]) {
+      switch (validator) {
+        case'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
   title: string;
-  description:  string;
+  @PositiveNumber
+  price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
 }
 
-function createCourseGoal(title: string, description: string): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {}; 
-  courseGoal.title = title;
-  courseGoal.description = description;
-  return courseGoal as CourseGoal;
-}
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-const majors: Readonly<string[]> = ['NS', 'AI'];
-// majors.push('SE'); // push do not exits on type Readonly string[]
-// majors.pop(); //
+  const titleEl = (document.getElementById('title') as HTMLInputElement);
+  const priceEl = (document.getElementById('price') as HTMLInputElement);
+
+  const title = titleEl.value;
+  const price = parseFloat(priceEl.value);
+
+  const createdCourse = new Course(title, price);
+
+  if(!validate(createdCourse)) {
+    alert('Invalid input, please try again!')
+    return;
+  }
+
+  console.log(createdCourse);
+});
